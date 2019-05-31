ac7181feac95b2b77d6953cd7aa75a3e120a0011
Mayank Gupta <mayank.gupta04@nagarro.com>
2019-05-31 11:15:10 +0530

    Module 5 code complete

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,40 +1,50 @@
 <template>
+  <div>
+    <h1>{{title}}</h1>
     <div>
-        <h1>
-            {{title}}
-        </h1>
-        <ul>
-            <book-item v-for="book in books" :book = 'book'></book-item>
-        </ul>
+      <book-form @addbook='appendBook'></book-form>
     </div>
+    <ul>
+      <book-item v-for="book in books" :book="book"></book-item>
+    </ul>
+  </div>
 </template>
 
 <script>
-import BookItem from './BookItem';
+import BookItem from "./BookItem";
+import BookForm from "./BookForm";
+
 export default {
-    name: 'BookList',
-    components: {
-        BookItem
-    },
-    data() {
-        return {
-            title: 'All Books',
-            books: [
-                {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-                {title: 'American Gods', author: 'Neil Gaiman'},
-                {title: 'Amusing Ourselves to Death', author: 'Neil Postman'}
-            ]   
-        }   
+  name: "BookList",
+  components: {
+    BookItem,
+    BookForm
+  },
+  data() {
+    return {
+      title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+      ]
+    };
+  },
+  methods: {
+    appendBook: function(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
     }
-}
+  }
+};
 </script>
 
 <style>
-h1, h2 {
-    font-weight: normal;
+h1,
+h2 {
+  font-weight: normal;
 }
 ul {
-    list-style-type: none;
-    padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 </style>
